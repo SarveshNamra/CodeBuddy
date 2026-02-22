427967eb375bb8480261ad8f0c780f172ab80ecf
SarveshNamra <sarveshcnamra@gmail.com>
2026-02-22 21:06:19 +0530

    Refactor package.json and package-lock.json for dependency organization; update db.js to improve PrismaClient initialization

--- backend/src/libs/db.js
+++ backend/src/libs/db.js
@@ -1,14 +1,26 @@
-import { PrismaClient } from "../generated/prisma/index.js";
+// import { PrismaClient } from "../generated/prisma/index.js";
 
-// Node JS doesn't have access to globals by default, so we use globalThis
+// // Node JS doesn't have access to globals by default, so we use globalThis
+// const globalForPrisma = globalThis;
+
+// const prisma = globalForPrisma.prisma ?? new PrismaClient();
+// // new PrismaClient() -> We are making new instance of PrismaClient(i.e. constructor)
+
+// if(process.env.NODE_ENV !== "production") {
+//     globalForPrisma.prisma = prisma;
+// }
+
+// export const db = prisma;
+// export default db;
+
+import { PrismaClient } from "@prisma/client";
+
 const globalForPrisma = globalThis;
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
-// new PrismaClient() -> We are making new instance of PrismaClient(i.e. constructor)
+export const db =
+  globalForPrisma.db ||
+  new PrismaClient();
 
-if(process.env.NODE_ENV !== "production") {
-    globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.db = db;
 }
-
-export const db = prisma;
-export default db;
